6b4e078af33e3b04528942c3db801c56d21fb776
Eugene Myazin <meniam@gmail.com>
2018-04-17 15:27:45 +0300

    Small fix for naming

--- src/Meniam/OauthBundle/DependencyInjection/MeOauthExtension.php
+++ src/Meniam/OauthBundle/DependencyInjection/MeOauthExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class OauthExtension extends Extension
+class MeOauthExtension extends Extension
 {
     /**
      * @param array            $configs
--- src/Meniam/OauthBundle/OauthBundle.php
+++ src/Meniam/OauthBundle/OauthBundle.php
@@ -2,8 +2,13 @@
 
 namespace Meniam\OauthBundle;
 
+use Meniam\OauthBundle\DependencyInjection\MeOauthExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OauthBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new MeOauthExtension();
+    }
 }
